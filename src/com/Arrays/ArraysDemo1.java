e3516c0df35f0d0dec368bedf4eaf61162afdb4e
Anjali <Anjali@ANJALI>
2026-06-19 14:20:10 +0530

    java

---
+++ src/com/Arrays/ArraysDemo1.java
@@ -0,0 +1,27 @@
+package com.Arrays;
+
+public class ArraysDemo1 {
+
+	public static void main(String[] arys) {
+
+		int[] ages;
+		ages = new int[10];
+		ages[0] = 89;
+		ages[1] = 98;
+		ages[2] = 67;
+		ages[3] = 22;
+		ages[4] = 23;
+		ages[5] = 65;
+		ages[6] = 26;
+		ages[7] = 28;
+		ages[8] = 20;
+		ages[9] = 24;
+
+		for (int i = 0; i < ages.length; i++) {
+			System.out.print(ages[i] + " ");
+
+		}
+
+	}
+
+}
